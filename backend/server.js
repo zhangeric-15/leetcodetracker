3a0264db7f0b5c381201d9aff3d30d81d16d70f2
Eric Zhang <71106422+zhangeric-15@users.noreply.github.com>
2025-05-27 23:53:29 -0400

    Added logic to connect to MongoDB database

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
-require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     console.log("GET request hit")
 })
 
-app.listen(process.env.PORT, () => {
-    console.log("Leetcode Tracker backend started and listening on port:", process.env.PORT)
+mongoose.connect(process.env.MONGO_URI)
+.then((response) => {
+    console.log("SUCCESS - Connected to MongoDB database")
+    app.listen(process.env.PORT, () => {
+        console.log("Leetcode Tracker backend started and listening on port:", process.env.PORT)
+    })
+})
+.catch(error => {
+    console.log("Error starting MongoDB database with error: ", error)
 })
