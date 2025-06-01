55ec5d94872d040b14ae87c89a8be1d29d97943e
Eric Zhang <71106422+zhangeric-15@users.noreply.github.com>
2025-06-01 18:12:48 -0400

    Added logic for creating user and sending back JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,26 @@
-const Test  = require("../models/userModel")
+const User  = require("../models/userModel")
+const jwt = require('jsonwebtoken')
+
+// paylod will be the user's _id property
+function generateToken(payload) {
+    const secretKey = process.env.JWT_SECRET_KEY;
+    // set expiration date on token
+    const options = {expiresIn: '1d'}
+    return jwt.sign({payload}, secretKey, options);
+}
 
 
 async function signupUser(req, res) {
     const { email, password } = req.body;
-    const user = await Test.signup(email, password);
+    try {
+        const user = await User.signup(email, password);
+        // Create JWT token
+        const token = generateToken(user._id);
+        return res.status(200).json({email, token})
+    } catch(error) {
+        console.log("Error signing up user: ", error.message)
+        return res.status(400).json({error: error.message})
+    }
 }
 
 module.exports = {
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -16,12 +17,14 @@ userSchema.statics.signup = async function(email, password) {
         throw Error("Email or Password is missing!");
     }
     // check if email already exists
-    const fetchedEmail = await this.find({ email });
-    if (fetchedEmail) {
+    const emailExists = await this.findOne({ email });
+    if (emailExists) {
         throw Error("Email already exists!");
     }
-    return null;
-
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = await this.create({email, password: hashedPassword});
+    return user;
 }
 
 const User = mongoose.model('User', userSchema);
