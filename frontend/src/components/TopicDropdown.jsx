4c5251d4b60784cce163b8dbe20647ec222e469e
Eric Zhang <71106422+zhangeric-15@users.noreply.github.com>
2025-07-15 21:31:34 -0700

    Added functionality for closing topic dropdown when clicked outside

--- frontend/src/components/TopicDropdown.jsx
+++ frontend/src/components/TopicDropdown.jsx
@@ -1,16 +1,40 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function TopicDropDown() {
     const [isTopicMenuOpen, setTopicMenuOpen] = useState(false);
+
+    // IMPORTANT: 
+    // Object returned by 'useRef' has a 'current' property. The initial value is set to the argument passed into 'useRef'
+    // Updating the object's 'current' property will NOT trigger a re-render. This is a variable that doesn't change during re-renders.
+    // When linked with a component like <div> that, the 'current' property will be set to the that DOM node AFTER it gets rendered on screen.
+    const dropDownMenuRef = useRef(null);
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return (() => {
+            console.log("REMOVING outside mousedown listener for Topic dropdown")
+            document.removeEventListener('mousedown', handleClickOutside)
+        })
+    }, [])
 
     function handleDropdownOpen() {
         console.log("Form opened");
         setTopicMenuOpen(true);
     }
+
+    // Handles any mouse click OUTSIDE of the dropdown menu
+    function handleClickOutside(event) {
+        // dropDownMenuRef.current.contains(event.target) checks if the user clicked INSIDE the dropdown menu
+        // Adding a ! means we want to make sure the user clicked outside so we can CLOSE it
+        if (dropDownMenuRef.current && !dropDownMenuRef.current.contains(event.target)) {
+            console.log("Detected outside of Topic Dropdown Menu. Closing it!");
+            setTopicMenuOpen(false);
+
+        }
+    }
     return (
         <div className="dropdown-container">
                 {isTopicMenuOpen ? (
-                    <div className="dropdown-menu">
+                    <div ref={dropDownMenuRef} className="dropdown-menu">
                         <div className="dropdown-value">
                             <span className="tag" style={{backgroundColor: 'Blue', borderRadius: '8px', padding: '6px', display: 'inline-block'}}>
                                 Default
